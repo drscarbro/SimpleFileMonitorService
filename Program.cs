4c3acf22dd8c668bc7b23ced5b93601fb57ae06d
Daniel Scarbrough <drscarbro@gmail.com>
2020-07-07 11:02:13 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
+using SimpleFileMonitor.Services;
+using SimpleFileMonitor.Services.Interfaces;
+using System;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+
+namespace SimpleFileMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .UseWindowsService()
+            .ConfigureServices((hostContext, services) => 
+                services.AddHostedService<FileMonitor>());
+    }
+}
---
+++ Services/FileMonitor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SimpleFileMonitor.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleFileMonitor.Services
+{
+    class FileMonitor : BackgroundService, IFileMonitor
+    {
+        private readonly ILogger<FileMonitor> _logger;
+
+        public FileMonitor(ILogger<FileMonitor> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
